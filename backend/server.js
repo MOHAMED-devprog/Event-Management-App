5c64794537628d63c8ac50c95c2fac6734676690
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-05-17 22:42:00 +0100

    updating routes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const imagesFolder = path.join(__dirname, "images");
 
 app.use(cors({methods : ["GET", "POST", "DELETE", "PUT"]}));
-app.use('/images', express.static(imagesFolder));
+app.use('/api/images', express.static(imagesFolder));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, "images/"),
@@ -19,11 +19,11 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 
-app.post('/images', upload.single("image"), (req, res) => {
-    res.json({url : `http://localhost:3000/images/${req.file.filename}`});
+app.post('/api/images', upload.single("image"), (req, res) => {
+    res.json({url : `/api/images/${req.file.filename}`});
 });
 
-app.delete('/delete/:filename', (req, res) => {
+app.delete('/api/delete/:filename', (req, res) => {
     const filename = req.params.filename;
     const imagePath = path.join(imagesFolder, filename);
 

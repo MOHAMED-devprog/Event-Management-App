8ba4c4b9ba81293285f9f66a85959441dcc6d94f
MOHAMED EL OUAFIR <mohamedoubaid999@gmail.com>
2025-05-18 19:28:35 +0100

    implementing the reminder notification

--- backend/reminderEvents.js
+++ backend/reminderEvents.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 const nodemailer = require("nodemailer");
-const serviceAccount = require('./firebase-key.json');
+const serviceAccount = require('./firebaseKeyAdmin.json');
 require('dotenv').config();
 
 admin.initializeApp({
